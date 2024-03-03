02d174babc5b1baece73357cbe92196b030540ad
jauhari06 <112462256+jauhari06@users.noreply.github.com>
2024-03-03 23:21:07 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 from io import BytesIO
 
 
-order_payment_df = pd.read_csv(r'/home/adminuser/data/order_payments_dataset.csv')
-products_df = pd.read_csv(r'/home/adminuser/data/products_dataset.csv')
+order_payment_df = pd.read_csv(r'data/order_payments_dataset.csv')
+products_df = pd.read_csv(r'data/products_dataset.csv')
 
 def create_payment_value_df(df):
     # Assuming order_id uniquely identifies each order
